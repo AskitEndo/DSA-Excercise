c74792c20f5a753f65c62f1a18416ed25a89e237
Ankit Kumar Singh <160294709+AskitEndo@users.noreply.github.com>
2024-04-27 23:55:38 +0530

    Day 2

---
+++ Codes/Day 2/reverseBits.cpp
@@ -0,0 +1,26 @@
+// This is A modearte difficulty problem which deals with bit manipulation
+// Link: https://www.naukri.com/code360/problems/reverse-bits_2181102
+
+#include<iostream>
+using namespace std;
+
+long reverseBits(long n){
+    long value;
+    int i = 0;
+    while (i < (sizeof(long) * 8)) {
+    long lsb = n & 1L;
+    value <<= 1;
+    value |= lsb;
+    n >>= 1;
+    i++;
+  }
+    return value;
+}
+
+int main()
+{
+    long n;
+    cin>>n;
+    cout<<reverseBits(n);
+    return 0;
+}
