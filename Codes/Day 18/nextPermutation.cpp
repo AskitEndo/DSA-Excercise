dfb97e4ae760b839f638f5e1029bb4fd1d87a9af
Ankit Kumar Singh <160294709+AskitEndo@users.noreply.github.com>
2024-05-13 23:14:18 +0530

    Day 18

--- Codes/Day 18/nextPermutation.cpp
+++ Codes/Day 18/nextPermutation.cpp
@@ -5,23 +5,18 @@
 using namespace std;
 
 
-// void nextPermutation(vector<int>& nums) {
-//     int n = nums.size();
-//     int i = n - 2;
-//     while (i >= 0 && nums[i] >= nums[i+1]) i--;
-//     if (i >= 0) {        
-//         int j = n - 1;
-//         while (j >= 0 && nums[j] <= nums[i]) j--;
-//         swap(nums[i], nums[j]);
-//     }
-//     reverse(nums.begin() + i + 1, nums.end());
-// }
-
-void nextPermutation(vector<int>& nums){
-
-
-
+void nextPermutation(vector<int>& nums) {
+    int n = nums.size();
+    int i = n - 2;
+    while (i >= 0 && nums[i] >= nums[i+1]) i--;
+    if (i >= 0) {        
+        int j = n - 1;
+        while (j >= 0 && nums[j] <= nums[i]) j--;
+        swap(nums[i], nums[j]);
+    }
+    reverse(nums.begin() + i + 1, nums.end());
 }
+
 
 
 int main()
