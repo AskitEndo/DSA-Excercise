78996a7d6f9490e63d3d818f4461f283e59a1dac
Ankit Kumar Singh <160294709+AskitEndo@users.noreply.github.com>
2024-05-15 21:16:48 +0530

    Day 19

--- Codes/Day 20/longestConsecutive.cpp
+++ Codes/Day 20/longestConsecutive.cpp
@@ -1,9 +1,27 @@
 #include<bits/Stdc++.h>
 using namespace std;
 
-
+int longestConsecutive(vector<int>& nums) {
+    if(nums.empty()) return 0;
+    sort(nums.begin(), nums.end());
+    int longest = 1;
+    int current = 1;
+    for(int i = 1; i < nums.size(); i++) {
+        if(nums[i] != nums[i - 1]) {
+            if(nums[i] == nums[i - 1] + 1) {
+                current++;
+            } else {
+                longest = max(longest, current);
+                current = 1;
+            }
+        }
+    }
+    return max(longest, current);
+}
 
 int main()
 {
+    vector<int> res = {9,1,4,7,3,-1,0,5,8,-1,6};
+    cout<<longestConsecutive(res);
     return 0;
 }
