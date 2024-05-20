7674e7afae3e47e9be7299b4b163b0c35f80b60a
Ankit Kumar Singh <160294709+AskitEndo@users.noreply.github.com>
2024-05-20 21:48:34 +0530

    Day 25

--- Codes/Day 25/spiralMatrix.cpp
+++ Codes/Day 25/spiralMatrix.cpp
@@ -1,4 +1,4 @@
-// Print MAtrix in spiral manner
+// Print Matrix in spiral manner
 // Link: https://leetcode.com/problems/spiral-matrix/description/
 
 #include <bits/Stdc++.h>
@@ -6,6 +6,50 @@ using namespace std;
 
 vector<int> spiralOrder(vector<vector<int>> &matrix)
 {
+
+    int numRow = matrix.size();
+    int numCol = matrix[0].size();
+    int left = 0;
+    int right = numCol - 1;
+    int top = 0;
+    int bottom = numRow - 1;
+
+    vector<int> ans;
+
+    while (top <= bottom && left <= right)
+    {
+        for (int i = left; i <= right; i++)
+        {
+            ans.push_back(matrix[top][i]);
+        }
+        top++;
+
+        for (int i = top; i <= bottom; i++)
+        {
+            ans.push_back(matrix[i][right]);
+        }
+        right--;
+
+        if (top <= bottom)
+
+        {
+            for (int i = right; i >= left; i--)
+            {
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        if (left <= right)
+        {
+            for (int i = bottom; i >= top; i--)
+            {
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    return ans;
 }
 
 int main()
