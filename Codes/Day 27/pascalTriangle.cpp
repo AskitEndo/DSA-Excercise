7dda13db47f3d478bd0acf7a5706be334cff3a49
Ankit Kumar Singh <160294709+AskitEndo@users.noreply.github.com>
2024-05-22 22:18:26 +0530

    Day 27

--- Codes/Day 27/pascalTriangle.cpp
+++ Codes/Day 27/pascalTriangle.cpp
@@ -4,11 +4,46 @@
 #include <bits/Stdc++.h>
 using namespace std;
 
+vector<int> generateRow(int row)
+{
+    long long ans = 1;
+    vector<int> ansrow;
+    ansrow.push_back(1);
+
+    for (int col = 1; col <= row; col++)
+    {
+        ans = ans * (row - col + 1) / col;
+        ansrow.push_back(ans);
+    }
+
+    return ansrow;
+}
+
 vector<vector<int>> generate(int numRows)
 {
+    vector<vector<int>> arr;
+
+    for (int i = 0; i < numRows; i++)
+    {
+        arr.push_back(generateRow(i));
+    }
+
+    return arr;
 }
 
 int main()
 {
+    int numRows = 5;
+    vector<vector<int>> pascalTriangle = generate(numRows);
+
+    for (const auto &row : pascalTriangle)
+    {
+        for (int num : row)
+        {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
